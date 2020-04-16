4e6e9a7b13f90f1005621dcf7ca646537e8ba4d4
Arsenii Baskakov <murmans1996@gmail.com>
2020-04-16 22:43:29 +0300

    Same but with Jquery

--- pyramide_slide.js
+++ pyramide_slide.js
@@ -1,74 +1,58 @@
+'use strict'
+//jquery usage practise
 
 console.log("!!!!");
 
-let pyramidPlace = document.getElementsByClassName("pyramide")[0];
+let pyramidPlace = $(".pyramide").eq(0);
 
-let levelRange = document.getElementsByName("levelRange")[0];
-let levelRange_lbl = document.getElementsByName("levelRange_lbl")[0];
-levelRange_lbl.innerText = levelRange.value;
-
-let brick_type = document.getElementsByName("brick_type")[0];
-
-printPyramid(levelRange.value, pyramidPlace, brick_type.value);
-
-levelRange.addEventListener("change", (e) => { levelRange_lbl.innerText = e.target.value;
-                                               removeAllChilds(pyramidPlace);
-                                               printPyramid(e.target.value, pyramidPlace, brick_type.value);
-                                                });
+let levelRange = $(document.getElementsByName("levelRange")[0]);
+let levelRange_lbl = $(document.getElementsByName("levelRange_lbl")[0]);
+levelRange_lbl.text(levelRange.val());
 
 
-brick_type.addEventListener("change", (e) => { removeAllChilds(pyramidPlace);
-                                              printPyramid(levelRange.value, pyramidPlace, e.target.value);
-                                            });
+let brick_type = $(document.getElementsByName("brick_type")[0]);
+
+printPyramid(levelRange.val(), pyramidPlace, brick_type.val());
 
 
+levelRange.on("change", (e) => { levelRange_lbl.text(e.target.value);
+    console.log(e.target.value);
+    pyramidPlace.empty();
+    printPyramid(e.target.value, pyramidPlace, brick_type.val());
+     });
 
-function removeAllChilds(node) {
-    while (node.lastElementChild) {
-        node.removeChild(node.lastElementChild);
-    }
-}
+
+brick_type.on("change", (e) => { pyramidPlace.empty();
+    printPyramid(levelRange.val(), pyramidPlace, e.target.value);
+  });
 
 function printPyramid(height, node, block_type) {
     let levels = height;
     let cur_block_nmb = 2;
 
-    //let pyramidPlace = document.getElementById("pyramid");
-    //let pyramidPlace = document.getElementsByClassName("pyramide")[0];
     let pyramidPlace = node;
-    let white_block = document.createElement("div");
-    white_block.classList.add("white_block");
-    // let blueviolet_block = document.createElement("div")
-    // blueviolet_block.classList.add("blueviolet_block");
-    let block = document.createElement("div")
-    block.classList.add(block_type);
 
     if(levels == 0) return;
 
     while(levels){
-
-        let line_div = document.createElement("div");
+        let line_div = $('<div></div>');
         for(let i = 0; i < levels - 1; i++){
-            let white_block = document.createElement("div");
-            white_block.classList.add("white_block");
-            line_div.appendChild(white_block);
+            let white_block = $('<div></div>');
+            white_block.addClass("white_block");
+            line_div.append(white_block);
         }
 
         for(let i = 0; i < cur_block_nmb; i++){
-            // let blueviolet_block = document.createElement("div");
-            // blueviolet_block.classList.add("blueviolet_block");
-            // line_div.appendChild(blueviolet_block);
-            let block = document.createElement("div");
-            block.classList.add(block_type);
-            line_div.appendChild(block);
+            let block = $('<div></div>');
+            block.addClass(block_type);
+            line_div.append(block);
         }
 
-        pyramidPlace.appendChild(line_div)
+        pyramidPlace.append(line_div)
 
         console.log(" ".repeat(levels - 1) + "#".repeat(cur_block_nmb));
 
         cur_block_nmb++;
         levels--
     }
-    //pyramidPlace.removeChild(document.getElementById("construction"));
 }
